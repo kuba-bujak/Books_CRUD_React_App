df5c889d833caf9f55af64fd823efe89b2ff53a1
Jakub Bujak <kuba.bujak0@wp.pl>
2023-03-30 20:37:53 +0200

    Created Edit and random img

--- src/App.js
+++ src/App.js
@@ -21,9 +21,22 @@ function App() {
         setBooks(updatedBooks);
     }
 
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return { ...book, title: newTitle };
+            }
+
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    }
+
     return (
         <div className='app'>
-            <BookList books={books} onDelete={deleteBookById } />
+            <h1>Reading List</h1>
+            <BookList books={books} onDelete={deleteBookById } onEdit={editBookById} />
             <BookCreate  onCreate={createBook} />
         </div>
     )
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import BookEdit from "./BookEdit";
 
-function BookShow({ book, onDelete }) {
+function BookShow({ book, onDelete, onEdit }) {
     const [showEdit, setShowEdit] = useState(false);
 
     const handleEditClick = () => {
@@ -12,12 +12,18 @@ function BookShow({ book, onDelete }) {
         onDelete(book.id);
     }
 
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    }
+
     let content = <h3>{book.title}</h3>;
     if (showEdit) {
-        content = <BookEdit />
+        content = <BookEdit book={book} onSubmit={handleSubmit} />
     }
 
     return <div className="book-show">
+            <img alt='books' src={`https://picsum.photos/seed/${book.id}/300/200`} />
             <div>{content}</div>
             <div className="actions">
                 <button className="edit" onClick={handleEditClick}>
