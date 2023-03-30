d200d87fbc244330bb47509ba08e811b0829a0c8
Jakub Bujak <kuba.bujak0@wp.pl>
2023-03-30 22:37:39 +0200

    Komunikacja z serwerem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 function App() {
     const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
     const deleteBookById = (id) => {
         const updatedBooks = books.filter((book) => {
@@ -13,11 +24,16 @@ function App() {
         setBooks(updatedBooks);
     }
 
-    const createBook = (title) => {
-        const updatedBooks = [...books, { 
-            id: Math.round(Math.random() * 9999), 
-            title 
-        }];
+    const createBook = async (title) => {
+
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+
+        const updatedBooks = [
+            ...books, 
+            response.data
+        ];
         setBooks(updatedBooks);
     }
 
